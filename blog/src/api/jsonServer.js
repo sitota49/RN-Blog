cbdf3f7b13399a9092fd067e8181552ddb8ed350
sitota49 <52606757+sitota49@users.noreply.github.com>
2020-08-04 15:25:39 +0300

    CRUD Operations and API Data Sync

---
+++ blog/src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL : 'http://466623e09f28.ngrok.io'
+});
---
+++ blog/src/screens/indexScreen.js
@@ -0,0 +1,75 @@
+import React, {useContext, useEffect} from 'react';
+import { StyleSheet, Text, View, FlatList, Button, TouchableOpacity} from 'react-native';
+import {Context} from '../context/BlogContext';
+import {Feather} from '@expo/vector-icons'
+
+const IndexScreen=({navigation})=>{
+    const {state, deleteBlogPost, getBlogPosts}= useContext(Context);
+
+    useEffect(()=>{
+        getBlogPosts();
+        const listener= navigation.addListener('didFocus', ()=>{
+            getBlogPosts();
+        });
+        // if index screen is removed from hierarchy
+        return()=>{
+            listener.remove();
+        };
+    },[])
+
+    return(
+        <View>         
+                <FlatList
+                data={state}
+                keyExtractor = {(blogPost) => blogPost.title}
+                renderItem={({item})=>{
+                         return (
+                         <TouchableOpacity onPress={()=>{
+                             navigation.navigate('Show', {
+                                 id: item.id
+                             })
+                         }}>
+                         <View style={styles.row}>
+                          <Text style={styles.title}>{item.title}</Text>
+                          <TouchableOpacity onPress={()=>{deleteBlogPost(item.id)}
+                              }>
+                          <Feather style={styles.icon} name="trash"/>
+                          </TouchableOpacity>
+                          </View>
+                          </TouchableOpacity>)
+                    }
+                } />
+        </View>
+    );
+};
+
+IndexScreen.navigationOptions= ({navigation})=>{
+    return {
+        headerRight : ()=> 
+        <TouchableOpacity onPress={ ()=> navigation.navigate('Create') }>
+        <Feather  
+        name='plus'
+        size={30}
+        />
+        </TouchableOpacity>
+    };
+};
+
+const styles= StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        borderTopWidth: 1,
+        borderColor : 'gray',
+        paddingHorizontal:10
+    },
+    title:{
+        fontSize:18
+    },
+    icon: {
+        fontSize: 24
+    }
+});
+
+export default IndexScreen;
